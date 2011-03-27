fe3bebcf9d0bfb0235ba357ed77f7bd4eb9bf974
Will Ross <paxswill@gmail.com>
2011-03-27 19:01:44 -0400

    Fix an incorrect assumption about length vs number of bytes

--- wavelet.c
+++ wavelet.c
@@ -135,7 +135,7 @@ double * createPaddedInput(double *unpadded, int length, int padding){
 	// Create the new array
 	double *padded = (double *)malloc(sizeof(double) * (length + padding + padding));
 	// Copy the old data in
-	memcpy(padded + padding, unpadded, length);
+	memcpy(padded + padding, unpadded, length * sizeof(double));
 	// Fill in the padding
 	for(int i = 0; i < padding; ++i){
 		// Front
