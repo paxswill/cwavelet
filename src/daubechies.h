22625956d0c4efdbd72a9b7b2fe23c2da6bd8f5a
Will Ross <paxswill@gmail.com>
2011-04-01 04:51:36 -0400

    Update the Daubechies and Haar wavelets for the new container

--- src/daubechies.h
+++ src/daubechies.h
@@ -31,10 +31,10 @@ double daubechies_8_scaling(circular_array *arr, int i);
 double daubechies_10_wavelet(circular_array *arr, int i);
 double daubechies_10_scaling(circular_array *arr, int i);
 
-static const wavelet DAUBECHIES_2_WAVELET = {daubechies_2_wavelet, daubechies_2_scaling, 2, 2};
-static const wavelet DAUBECHIES_4_WAVELET = {daubechies_4_wavelet, daubechies_4_scaling, 2, 4};
-static const wavelet DAUBECHIES_6_WAVELET = {daubechies_6_wavelet, daubechies_6_scaling, 2, 6};
-static const wavelet DAUBECHIES_8_WAVELET = {daubechies_8_wavelet, daubechies_8_scaling, 2, 8};
-static const wavelet DAUBECHIES_10_WAVELET = {daubechies_10_wavelet, daubechies_10_scaling, 2, 10};
+static const wavelet DAUBECHIES_2_WAVELET = {{.wavelet = daubechies_2_wavelet}, {.scaling = daubechies_2_scaling}, false, 2, 2};
+static const wavelet DAUBECHIES_4_WAVELET = {{.wavelet = daubechies_4_wavelet}, {.scaling = daubechies_4_scaling}, false, 2, 4};
+static const wavelet DAUBECHIES_6_WAVELET = {{.wavelet = daubechies_6_wavelet}, {.scaling = daubechies_6_scaling}, false, 2, 6};
+static const wavelet DAUBECHIES_8_WAVELET = {{.wavelet = daubechies_8_wavelet}, {.scaling = daubechies_8_scaling}, false, 2, 8};
+static const wavelet DAUBECHIES_10_WAVELET = {{.wavelet = daubechies_10_wavelet}, {.scaling = daubechies_10_scaling}, false, 2, 10};
 
 #endif /* end of include guard: DAUBECHIES_H_4IFSHX8 */
