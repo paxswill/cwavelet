21f51b45782825a69ed64325923322d312c869b1
Will Ross <paxswill@gmail.com>
2011-04-15 03:34:32 -0400

    This was wrong, now it is right

--- src/haar.c
+++ src/haar.c
@@ -10,7 +10,7 @@
 #include "haar.h"
 
 double haar_wavelet(circular_array *arr, int i){
-	return (ca_get(arr, i) - ca_get(arr, i + 1)) / 1;
+	return (ca_get(arr, i) - ca_get(arr, i + 1)) / 2;
 }
 
 double haar_scaling(circular_array *arr, int i){
