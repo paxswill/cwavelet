9410871b514c259d3ebc237c1ec7371127a5ed9a
Will Ross <paxswill@gmail.com>
2011-04-16 03:37:38 -0400

    Allow the user to over-ride the default benchmark

--- tests/splitTest.c
+++ tests/splitTest.c
@@ -1,9 +1,16 @@
+#include <stdlib.h>
 #include "wavelet.h"
 
-int main(){
+
+int main(int argc, char **argv){
+	int offset = 23;
+	if(argc > 1){
+		//get the number of the next argument
+		offset = atoi(argv[1]);
+	}
 	int correct = 0;
 	// Loop here later
-	int length = 1 << 23;
+	int length = 1 << offset;
 	double *arr = malloc(sizeof(double) * length);
 	for(int i = 0; i < length; ++i){
 		arr[i] = i;
