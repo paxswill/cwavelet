7fccb73ac934a0be246dc524e50db8c9cea1499f
Will Ross <paxswill@gmail.com>
2011-04-16 02:43:45 -0400

    Clarify testSplit for generalized testing

--- tests/splitTest.c
+++ tests/splitTest.c
@@ -1,18 +1,14 @@
 #include "wavelet.h"
 
-extern int threadCount;
 int main(){
 	int correct = 0;
 	// Loop here later
-	int length = 1 << 8;
+	int length = 1 << 23;
 	double *arr = malloc(sizeof(double) * length);
 	for(int i = 0; i < length; ++i){
 		arr[i] = i;
 	}
-	printf("thread count: %d\n", threadCount);
 	liftSplit(arr, length);
-	printf("thread count: %d\n", threadCount);
 	liftMerge(arr, length);
-	printf("thread count: %d\n", threadCount);
 	return 0;
 }
